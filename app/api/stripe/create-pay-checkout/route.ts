dec09b8abf2a4269f9921d6f287376d2ecf72d3b
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-04-16 16:38:05 -0300

    add route create-pay-checkout

---
+++ app/api/stripe/create-pay-checkout/route.ts
@@ -0,0 +1,33 @@
+import stripe from '@/app/lib/stripe';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest){
+    const { testeId, useEmail } = await req.json();
+
+    const price = process.env.STRIPE_PRODUCT_PRICE_ID;
+
+    if (!price) {
+        return NextResponse.json({ error: 'Price ID not found' }, { status: 500 });
+    }
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            mode: 'payment',
+            line_items: [
+                {
+                    price,
+                    quantity: 1,
+                },
+            ],
+            payment_method_types: ['card', 'boleto'],
+            success_url: `${req.headers.get('origin')}/success`,
+            cancel_url: `${req.headers.get('origin')}/`,
+        });
+
+        return NextResponse.json({ sessionId: session.id }, { status: 200 });
+
+
+    } catch (error) {
+        console.error(error);
+    }
+}
