b7fb437d4868b6985b73997ba255fa8482af8395
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-04-16 22:55:16 -0300

    add route create-subscripiton-checkout

--- app/api/stripe/create-subscription-checkout/route.ts
+++ app/api/stripe/create-subscription-checkout/route.ts
@@ -1,44 +1,37 @@
 import stripe from '@/app/lib/stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest){
-    const { testeId, useEmail } = await req.json();
+export async function POST(req: NextRequest) {
+    const { testeId } = await req.json();
 
-    const price = process.env.STRIPE_PRODUCT_PRICE_ID;
+    const price = process.env.STRIPE_SUBSCRIPTION_PRICE_ID;
 
     if (!price) {
         return NextResponse.json({ error: 'Price ID not found' }, { status: 500 });
     }
-
     const metadata = {
         testeId,
     }
 
-    try {
+    try{
         const session = await stripe.checkout.sessions.create({
-            mode: 'payment',
+            mode: 'subscription',
             line_items: [
                 {
                     price,
                     quantity: 1,
                 },
             ],
-            payment_method_types: ['card', 'boleto'],
+            payment_method_types: ['card'],
             success_url: `${req.headers.get('origin')}/success`,
             cancel_url: `${req.headers.get('origin')}/`,
-            ...(useEmail && {customer_email: useEmail}),
             metadata,
+            
         });
 
-        if(!session){
-            return NextResponse.json({ error: 'Session not found' }, { status: 500 });
-        }
-
-        return NextResponse.json({ sessionId: session.id }, { status: 200 });
-
-
-    } catch (error) {
+    }catch (error) {
         console.error(error);
         return NextResponse.error();
     }
+
 }
